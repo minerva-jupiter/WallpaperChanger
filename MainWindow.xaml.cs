2f99c6c6cd0b5d8fc78cfd969f63d8caf9755ceb
Minerva_juppiter <94231606+minerva-jupiter@users.noreply.github.com>
2022-05-18 23:12:41 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WallpaperChanger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //変数宣言
+        string wherePictureFile;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Set_Click(object sender, RoutedEventArgs e)
+        {
+            using (var cofd = new CommonOpenFileDialog()
+            {
+                Title = "Select pictures Folder",
+                InitialDirectory = @"C:Users/",
+                // フォルダ選択モードにする
+                IsFolderPicker = true,
+            })
+            {
+                if (cofd.ShowDialog() != CommonFileDialogResult.Ok)
+                {
+                    return;
+                }
+
+                // FileNameで選択されたフォルダを取得する
+                MessageBox.Show("you selected",$"{cofd.FileName}");
+
+                //Textboxを選択されたフォルダーに変更
+                wherePictureFile = cofd.FileName;
+                WherePicturesFolder.Text = wherePictureFile;
+            }
+        }
+
+        private void Run_Click(object sender, RoutedEventArgs e)
+        {
+            if(wherePictureFile == null)
+            {
+                MessageBox.Show("Folder wasn't selected!");
+            }
+            else
+            {
+                string command = "";
+                Process.Start("cmd.exe", command);
+            }
+            
+        }
+    }
+}
